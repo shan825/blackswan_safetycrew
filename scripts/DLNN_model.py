a329ba63bf12677dd0f770c6848e5c12378899cc
doug-cady <douglas.cady@posteo.net>
2022-08-30 12:06:51 -0500

    Added python program to simulate 800 rows of model data.

---
+++ scripts/DLNN_model.py
@@ -0,0 +1,13 @@
+"""
+
+
+"""
+
+import pandas as pd
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import statistics
+
+
+sc = StandardScaler()
