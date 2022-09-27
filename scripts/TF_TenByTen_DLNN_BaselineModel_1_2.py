7f60703f6ba0fe69412ff76f4dce6425b541f5c0
Kelly Johnson <73440087+kjohn22@users.noreply.github.com>
2022-09-27 18:29:14 -0400

    Update TF_TenByTen_DLNN_BaselineModel_1_2.py
    functional TF baseline model that actually ran!

--- scripts/TF_TenByTen_DLNN_BaselineModel_1_2.py
+++ scripts/TF_TenByTen_DLNN_BaselineModel_1_2.py
@@ -10,25 +10,23 @@ https://machinelearningmastery.com/multi-class-classification-tutorial-keras-dee
 """
 
 
-# multi-class classification with Keras
 import numpy as np
-import random
-import time
-import pandas as pd
 import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasClassifier
-from keras.utils import np_utils
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
+import pandas as pd
+import sklearn
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
-from sklearn.pipeline import Pipeline
+from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
-import xlsxwriter
-from datetime import datetime
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+sc = StandardScaler()
+import statistics
 
-# check gpu config and see if a minimum of 7 compute power
+# check gpu config and capabilities
 # =============================================================================
 # print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 # from tensorflow.python.client import device_lib
@@ -41,19 +39,10 @@ from datetime import datetime
 # print(is_cuda_gpu_min_7)
 # =============================================================================
 
+# load data
+data = pd.read_csv("tenByTenModelData.csv")
+dataset = data.values
 
-# input data
-# INPUT_DATA_PATH = "norm_tenByTenModelData.csv"
-
-# initiate clock
-# =============================================================================
-# TODAYS_DATETIME = datetime.now().strftime("%m-%d-%Y_%H%M%S")
-# TODAYS_DATE = datetime.now().strftime("%m-%d-%Y")
-# TODAYS_TIME = datetime.now().strftime("%H:%M:%S %Z")
-# =============================================================================
-# load dataset
-dataframe = pd.read_csv("tenByTenModelData.csv")
-dataset = dataframe.values
 X = dataset[:,0:3].astype(float)
 Y = dataset[:,3]
 
@@ -62,11 +51,24 @@ encoder = LabelEncoder()
 encoder.fit(Y)
 encoded_Y = encoder.transform(Y)
 # convert integers to dummy variables (i.e. one hot encoded)
-dummy_y = np_utils.to_categorical(encoded_Y)
- 
-# define baseline model
-def baseline_model():
-	# create model
+dummy_y = to_categorical(encoded_Y)
+
+np.shape(dummy_y)
+# test train split
+(X_train, X_test, Y_train, Y_test) = train_test_split(X,dummy_y,random_state=42, test_size=0.3)
+
+# normalizing data
+X_train = tf.keras.utils.normalize(X_train)
+X_test = tf.keras.utils.normalize(X_test)
+
+# checking shape of data
+print(X_train.shape)
+print(X_test.shape)
+
+# function to create and compile model
+def createModel():
+
+    # create model
 	model = Sequential()
 	model.add(Dense(512, input_dim=3,kernel_initializer='he_uniform', activation='relu'))
 	model.add(Dense(128, input_dim=3,kernel_initializer='he_uniform', activation='relu'))
@@ -75,8 +77,9 @@ def baseline_model():
 	# Compile model
 	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 	return model
- 
-estimator = KerasClassifier(build_fn=baseline_model, epochs=2500, batch_size=5, verbose=0)
+
+estimator = KerasClassifier(build_fn=createModel, epochs=2500, batch_size=5, verbose=0)
 kfold = KFold(n_splits=10, shuffle=True)
 results = cross_val_score(estimator, X, dummy_y, cv=kfold)
 print("Baseline Accuracy (Standard Deviation): %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+
