e3237a0c9dc20222c9f7f46de8364ab8a34eed15
MikeLim2000 <13158118+MikeLim2000@users.noreply.github.com>
2022-10-02 09:59:09 -0400

    Experimenting with 3D scatterplot and determining options for an animated GIF.

--- scripts/3d_scatter_animated.py
+++ scripts/3d_scatter_animated.py
@@ -2,10 +2,37 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import interactive
+interactive(True)
+
 # Used results from Doug's Model on 9-19-2022
 df = pd.read_csv("model_results\PyTorch_800caseResults__09-19-2022_163431.csv")
-print(df.head())
+#print(df.head())
 
+# Original 3D scatterplot code
+
+#Create X,Y, and Z variables
+#z = df['Target']
+#x = df['xTemp']
+#y = df['yVol']
+
+#fig = plt.figure()
+#ax = fig.add_subplot(111, projection='3d')
+
+#for s in df['CorrectPred'].unique():
+#    ax.scatter3D(df.xTemp[df['CorrectPred']==s],
+#               df.yVol[df['CorrectPred']==s],
+#               df.Target[df['CorrectPred']==s],label=s)
+#plt.title("3D Scatter of Temp Vol and Target")
+#plt.xlabel('Temperature')
+#plt.ylabel('Volume')
+#ax.set_zlabel('Target')    
+#ax.legend()
+#fig.show()
+
+# Use Plotly to create interactive 3D scatterplot of holdout
 fig = px.scatter_3d(df, 
                     x='xTemp', 
                     y='yVol', 
